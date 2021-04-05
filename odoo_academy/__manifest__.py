7b7f7b1463a9a238d1168688c6c405f924fdb7c5
Fernando Duce <fduce@maxsistechnologies.com>
2021-04-05 18:36:57 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,33 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Fernando',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+    
+}
